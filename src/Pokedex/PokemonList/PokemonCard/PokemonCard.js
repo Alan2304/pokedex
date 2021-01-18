d3924403abce7efa2d8e8121e903fda82470ce91
@sanchezalan97 <sanchezalan95@gmail.com>
2021-01-18 00:02:12 -0600

    missing parts of the code

---
+++ src/Pokedex/PokemonList/PokemonCard/PokemonCard.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+import { POKEMON_IMAGE } from "../../../constants";
+import "./PokemonCard.scss";
+
+const PokemonCard = () => {};
+
+export default PokemonCard;
---
+++ src/Pokedex/PokemonList/PokemonList.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+import "./PokemonList.scss";
+
+const PokemonList = () => {};
+
+export default PokemonList;
